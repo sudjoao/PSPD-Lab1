c5d1d97bdbed4ab7b4c857190fdb5a537a370cfe
Gabriel Davi Silva Pereira <gdavi.gd@gmail.com>
2022-11-25 17:24:33 -0300

    add user interface

--- RPC/word_counter_client.c
+++ RPC/word_counter_client.c
@@ -11,14 +11,6 @@
 #include <string.h>
 
 
-char * read_file(char*);
-char* copyString(char s[], int);
-CLIENT* create_client(char *host);
-pthread_t* start_workers(int, char *[]);
-void* worker_runner(void *);
-void wait_workers(int, pthread_t *);
-
-sem_t mutex;
 
 typedef struct {
 	char * host; 
@@ -28,35 +20,68 @@ typedef struct {
 
 result res = { 0, 0 };
 
+typedef struct {
+	char filename[256]; 
+	char word[256]; 
+	int nWorkers; 
+	char hosts[2][256]; 
+} Inputs;
+
+char * read_file(char*);
+char* copyString(char s[], int);
+CLIENT* create_client(char *host);
+pthread_t* start_workers(Inputs);
+void* worker_runner(void *);
+void wait_workers(int, pthread_t *);
+void get_user_input(Inputs* inputs); 
+
+sem_t mutex;
+
 int
 main (int argc, char *argv[])
 {
-    if (argc < 4) {
-        printf ("usage: %s word file host1 host2\n", argv[0]);
-        exit (1);
-    }
 
+	Inputs inputs; 
+	get_user_input(&inputs); 
+	pthread_t *tids = start_workers(inputs);
+	wait_workers(inputs.nWorkers, tids);	
 
-	int nWorkers = argc - 3;
-	pthread_t *tids = start_workers(nWorkers, argv + 1);
-	wait_workers(nWorkers, tids);	
-
-    printf("Ocorrencias da palavra %s: %d, Numero total de palavras no arquivo: %d \n", argv[1] ,res.occurrences, res.count);
+    printf("Ocorrencias da palavra %s: %d, Numero total de palavras no arquivo: %d \n", inputs.word ,res.occurrences, res.count);
     return 0;
 }
 
 
-pthread_t* start_workers(int nWorkers, char *argv[]) {
-    printf("start worder \n");  
-	pthread_t *tids = malloc(nWorkers * sizeof(pthread_t));
-    WorkerArgs *workersArgs = malloc(nWorkers * sizeof(WorkerArgs));
+void get_user_input(Inputs* inputs){
+	printf("Digite o nome do arquivo que deseja utilizar: \n"); 
+	scanf("%s" , inputs->filename);
 
-	char * word = argv[0]; 
-	char * filename = argv[1]; 
- 
-	char *buffer = read_file(filename);
+	printf("Digite o nome da palavra que seja saber a ocorrencia: \n"); 
+	scanf("%s" , inputs->word);
+
+	printf("Quantidade de workers (1 ou 2): \n"); 
+	scanf("%d", &inputs->nWorkers); 
+
+	if(inputs->nWorkers > 2 || inputs->nWorkers < 1){
+		printf("Quantidade de workers deve ser entre 1 e 2"); 
+	}
+
+	for(int i = 0; i < inputs->nWorkers; i++){
+		char temp[256]; 
+		printf("Nome do host numero %d\n", i); 
+		scanf("%s" , temp);
+		strcpy(inputs->hosts[i], temp); 
+	}
+}
+
+
+pthread_t* start_workers(Inputs inputs) {
+    printf("start worder \n");  
+	pthread_t *tids = malloc(inputs.nWorkers * sizeof(pthread_t));
+    WorkerArgs *workersArgs = malloc(inputs.nWorkers * sizeof(WorkerArgs));
+
+	char *buffer = read_file(inputs.filename);
 	int buffer_len = strlen(buffer); 
-	int word_len = strlen(word); 
+	int word_len = strlen(inputs.word); 
         
 	if(workersArgs == NULL || tids == NULL || buffer == NULL) {
         printf("Memomry error\n");
@@ -64,15 +89,15 @@ pthread_t* start_workers(int nWorkers, char *argv[]) {
     }
 
 
-	int workerDataLen = buffer_len / nWorkers; 
+	int workerDataLen = buffer_len / inputs.nWorkers; 
       
 	sem_init(&mutex, 0, 1);				
-	for (int i = 0; i < nWorkers; i++) {
-		workersArgs[i].host = argv[i + 2]; 
+	for (int i = 0; i < inputs.nWorkers; i++) {
+		workersArgs[i].host = inputs.hosts[i]; 
 
 		int offset = i*workerDataLen; 
 
-		workersArgs[i].op.word.word_val = word; 
+		workersArgs[i].op.word.word_val = inputs.word; 
 		workersArgs[i].op.data.data_val = buffer + offset; 
 		workersArgs[i].op.word.word_len = word_len; 
 		workersArgs[i].op.data.data_len = workerDataLen; 
@@ -121,15 +146,12 @@ char *read_file(char *filename)
   FILE *file;
 
   file = fopen(filename, "r");
-
   if (file == NULL) return NULL;
-
   fseek(file, 0, SEEK_END);
   int length = ftell(file);
   fseek(file, 0, SEEK_SET);
 
   char *string = malloc(sizeof(char) * (length+1));
-
   char c;
 
   int i = 0;
@@ -137,7 +159,6 @@ char *read_file(char *filename)
   fread(string, sizeof(char), length, file);
 
   fclose(file);
-
   return string;
 }
 
@@ -149,3 +170,4 @@ CLIENT* create_client(char *host) {
         }
 	return clnt;
 }
+
